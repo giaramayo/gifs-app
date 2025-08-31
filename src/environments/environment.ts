85a5d413f841f7e451bf0c713238c81536b3be91
garamayo <gisella.aramayo@besysoft.com>
2025-08-31 12:26:14 -0300

    Se agrega configuracion de environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+  giphyApiKey: 'gk94aLGrTB1kEtWBy5P8FgpPsc4Shusf',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
