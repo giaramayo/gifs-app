bf25f12429f7261e3863a3fe31d99b5b719f1b9e
garamayo <gisella.aramayo@besysoft.com>
2025-09-01 15:01:00 -0300

    [modificacion en estilos

--- src/app/gifs/components/gif-list/gif-list-item/gif-list-item.component.ts
+++ src/app/gifs/components/gif-list/gif-list-item/gif-list-item.component.ts
@@ -4,29 +4,32 @@ import { Component, input } from '@angular/core';
   selector: 'gif-list-item',
   template: `
     <div class="relative group">
-  <img
-    class="h-full w-full rounded-lg object-cover"
-    [src]="imageUrl()"
-    [alt]="title()"
-  />
+      <img
+        class="h-full w-full rounded-lg object-cover"
+        [src]="imageUrl()"
+        [alt]="title()"
+      />
+      <!-- Overlay con título -->
+      <div class="absolute bottom-0 left-0 w-full bg-black/70 text-white text-sm p-2 opacity-0 group-hover:opacity-100 transition">
+        {{ title() }}
+      </div>
 
-  <!-- Botón de copiar -->
-  <button
-    (click)="copyLink()"
-    class="absolute top-2 right-10 bg-black/70 text-white p-1 rounded opacity-0 group-hover:opacity-100 transition"
-  >
-    <i class="fa-regular fa-copy"></i>
-  </button>
+      <!-- Botón de copiar -->
+      <button
+        (click)="copyLink()"
+        class="absolute top-2 right-10 bg-black/70 text-white p-1 rounded opacity-0 group-hover:opacity-100 transition"
+      >
+        <i class="fa-regular fa-copy"></i>
+      </button>
 
-  <a
-    [href]="'https://wa.me/?text=' + [title() + ' ' + imageUrl()]"
-    target="_blank"
-    class="absolute top-2 right-2 bg-green-600 text-white p-1 rounded opacity-0 group-hover:opacity-100 transition"
-  >
-    <i class="fa-brands fa-whatsapp"></i>
-  </a>
-</div>
-
+      <a
+        [href]="'https://wa.me/?text=' + [title() + ' ' + imageUrl()]"
+        target="_blank"
+        class="absolute top-2 right-2 bg-green-600 text-white p-1 rounded opacity-0 group-hover:opacity-100 transition"
+      >
+        <i class="fa-brands fa-whatsapp"></i>
+      </a>
+    </div>
   `,
 })
 export class GifListItemComponent {
